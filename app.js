d1f7d3c8b8be3bd8ce3a206d5fdf39e44898cd10
Andrei Danut Mihai <138859815+andreidanutmihai85@users.noreply.github.com>
2024-05-19 22:24:15 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,181 @@
+const express = require('express');
+const mysql = require("mysql")
+const path = require("path")
+const dotenv = require('dotenv')
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcryptjs")
+var router = express.Router();
+
+const session = require('express-session');
+const { register } = require('module');
+
+//Nume label: dbnamelabel
+
+dotenv.config({ path: './.env'})
+
+const app = express();
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+})
+
+const publicDir = path.join(__dirname, './public')
+
+app.use(express.static(publicDir))
+app.use(express.urlencoded({extended: 'false'}))
+app.use(express.json())
+
+app.set('view engine', 'hbs')
+
+db.connect((error) => {
+    if(error) {
+        console.log(error)
+    } else {
+        console.log("MySQL connected!")
+    }
+})
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.get("/register", (req, res) => {
+    res.render("register")
+})
+
+app.get("/login", (req, res) => {
+    res.render("login")
+})
+
+
+// =================================REGISTER=============================
+
+app.post("/auth/register", (req, res) => {    
+    const { name, email, password, password_confirm } = req.body
+
+    db.query('SELECT email FROM users WHERE email = ?', [email], async (error, result) => {
+        if(error){
+            console.log(error)
+        }
+
+        if( result.length > 0 ) {
+            return res.render('register', {
+                message: 'This email is already in use'
+            })
+        } else if(password !== password_confirm) {
+            return res.render('register', {
+                message: 'Password Didn\'t Match!'
+            })
+        }
+
+        let hashedPassword = await bcrypt.hash(password, 8)
+
+        console.log(hashedPassword)
+       
+        db.query('INSERT INTO users SET?', {name: name, email: email, password: password}, (err, result) => {
+            if(error) {
+                console.log(error)
+            } else {
+                return res.render('register', {
+                    message: 'User registered!'
+                })
+            }
+        })        
+    })
+})
+
+
+//=======================================================LOGIN test 
+
+const connection = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
+
+
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'static')));
+
+// http://localhost:3000/
+app.get('/', function(request, response) {
+	// Render login template
+	response.sendFile(path.join(__dirname + '/login'));
+});
+
+// http://localhost:3000/auth
+app.post('/auth', function(request, response) {
+	// Capture the input fields
+	let username = request.body.email;
+	let password = request.body.password;
+	// Ensure the input fields exists and are not empty
+	if (username && password) {
+		// Execute SQL query that'll select the account from the database based on the specified username and password
+		connection.query('SELECT * FROM users WHERE email = ? AND password = ?', [username, password], function(error, results, fields) {
+			// If there is an issue with the query, output the error
+			if (error) throw error;
+			// If the account exists
+			if (results.length > 0) {
+				// Authenticate the user
+				request.session.loggedin = true;
+				request.session.email = username;
+				// Redirect to home page
+				response.redirect('/home');
+			} else {
+				response.send('Incorrect email and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter email and Password!');
+		response.end();
+	}
+});
+
+
+// http://localhost:3000/home
+app.get('/home', function(request, response) {
+	// If the user is loggedin
+	if (request.session.loggedin) {
+		// Output username
+
+
+		// response.send('Welcome back, ' + request.session.email + '!'); 
+
+        response.redirect('https://www.google.ro');
+
+	} else {
+		// Not logged in
+		response.send('Please login to view this page!');
+	}
+	response.end();
+});
+
+
+
+
+
+
+
+
+
+
+//=======================================================LOGIN test 
+
+
+    app.listen(5000, ()=> {
+        console.log("server started on port 5000")
+    })
+    
+
+
---
+++ site-ul cu info.md
@@ -0,0 +1,7 @@
+https://codeshack.io/basic-login-system-nodejs-express-mysql/
+
+MySQL Server >= 5.6
+Node.js 
+Express - Install with command:                     npm install express --save
+Express Sessions - Install with command:            npm install express-session --save
+MySQL for Node.js - Install with command:           npm install mysql --save
